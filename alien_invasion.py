c2a0d81ca29a5ebe4531b9ffd650e94a0bbdebb5
busranur <busranurok@gmail.com>
2023-03-17 20:50:34 +0300

    Settings Class Created. The settings have been exported to the main program file.
    Settings Class Created. The settings have been exported to the main program file.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,13 +1,14 @@
 #Oyuncu çıkış yaptığında oyundan çıkmak için sys modülündeki araçlar kullanılır.
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     """
     Oyunun değerlerini ve davranışını
     yönetmek için genel bir sınıf.
     """
-
+    
     def __init__(self):
         """
         Oyunu başlat ve
@@ -16,11 +17,10 @@ class AlienInvasion:
         #İlk değer ataması yapılır.
         pygame.init()
         
+        self.settings = Settings()
         #1200 piksel genişlik, 800 piksel yükseklik demeti.
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.width, self.settings.height))
         pygame.display.set_caption("Alien Invasion")
-        #Arka plan rengini ayarla.
-        self.bg_color = (230, 230, 230)
         
     def run_game(self):
         """Oyun için ana döngüyü başlat"""
@@ -30,8 +30,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             #Döngüden her geçişte ekranı yeniden çizdir.
-            self.screen.fill(self.bg_color)
-                    
+            self.screen.fill(self.settings.bg_color)
             #En son çizilen ekranı görünür yap.
             pygame.display.flip()
             
---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    """
+    Uzaylı istilası için bütün
+    ayarları saklayan bir sınıf.
+    """
+    def __init__(self):
+        """Oyunun ayarlarına ilk değer atayınız"""
+        #Ekran ayarları
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
