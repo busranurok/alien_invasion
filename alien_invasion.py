d004ae9cfc7b70a1a12ee35ec7a2cfe14250e05c
busranur <busranurok@gmail.com>
2023-03-17 23:41:50 +0300

    Restructuring has been performed. The _check_events() and _update_screen() methods have been written.
    Restructuring has been performed.
    The _check_events() and _update_screen() methods have been written.

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,7 +20,7 @@ class AlienInvasion:
         
         self.settings = Settings()
         #1200 piksel genişlik, 800 piksel yükseklik demeti.
-        self.screen = pygame.display.set_mode((self.settings.width, self.settings.height))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
         
         self.ship = Ship(self)
@@ -28,13 +28,23 @@ class AlienInvasion:
     def run_game(self):
         """Oyun için ana döngüyü başlat"""
         while True:
-            #Klavye ve fare olaylarını gözle.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
+        
+    #Python' da, baştaki tek _ (alt tire) yardımcı metodu belirler.
+    def _check_events(self):
+        """Klavye ve fare olaylarına yanıt ver."""
+        #Klavye ve fare olaylarını gözle.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+                
+    def _update_screen(self):
+        """Ekrandaki resimleri güncelle ve yeni ekrana dön."""
             #Döngüden her geçişte ekranı yeniden çizdir.
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
+            
             #En son çizilen ekranı görünür yap.
             pygame.display.flip()
             
